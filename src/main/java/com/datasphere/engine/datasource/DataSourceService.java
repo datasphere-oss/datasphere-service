d4ae9a33a09f2eab61c5b89a1dc02fd668f73f43
aladin-1001 <aladin1001@126.com>
2019-08-24 12:13:03 +0800

    add iBatis based datasource initializer
    add iBatis based datasource initializer

---
+++ src/main/java/com/datasphere/engine/datasource/DataSourceService.java
@@ -0,0 +1,35 @@
+package com.datasphere.engine.datasource;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import java.io.File;
+
+@Service
+public class DataSourceService {
+  private static final Logger log = LoggerFactory.getLogger(DataSourceService.class);
+  private HikariDataSource dataSource;
+
+  @PostConstruct
+  public void initialize() {
+    log.info("Initialize data source....");
+    HikariConfig config = new HikariConfig(System.getProperty("user.dir")+ File.separator+"dwresources"+File.separator+"hikari.properties");
+    dataSource = new HikariDataSource(config);
+  }
+
+  public HikariDataSource getDataSource() {
+    return dataSource;
+  }
+
+
+  @PreDestroy
+  public void releaseResource() {
+    log.info("release data source");
+    if(dataSource != null) dataSource.close();
+  }
+}
---
+++ src/main/java/com/datasphere/engine/datasource/MyBatisSqlSessionFactoryService.java
@@ -0,0 +1,45 @@
+package com.datasphere.engine.datasource;
+
+import org.apache.ibatis.mapping.Environment;
+import org.apache.ibatis.session.Configuration;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.apache.ibatis.transaction.TransactionFactory;
+import org.apache.ibatis.transaction.jdbc.JdbcTransactionFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import javax.sql.DataSource;
+
+@Service
+public class MyBatisSqlSessionFactoryService {
+  private static final Logger log = LoggerFactory.getLogger(MyBatisSqlSessionFactoryService.class);
+
+  @Autowired
+  private DataSourceService dataSourceService;
+
+  private SqlSessionFactory sqlSessionFactory;
+
+  @PostConstruct
+  public void initialize() {
+    DataSource dataSource = dataSourceService.getDataSource();
+    TransactionFactory transactionFactory = new JdbcTransactionFactory();
+    Environment environment  = new Environment("development", transactionFactory, dataSource);
+    Configuration configuration = new Configuration(environment);
+    configuration.addMappers("com.datasphere.engine.processor");
+    sqlSessionFactory = new SqlSessionFactoryBuilder().build(configuration);
+
+  }
+
+  public SqlSessionFactory getSqlSessionFactory() {
+    return sqlSessionFactory;
+  }
+
+  public SqlSession getSqlSession() {
+    return sqlSessionFactory.openSession();
+  }
+}
