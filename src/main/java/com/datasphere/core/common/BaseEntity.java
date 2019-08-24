144f76b9927df43f271394f09655c6c2396f707e
aladin-1001 <aladin1001@126.com>
2019-08-24 11:37:18 +0800

    add basic entity for Engine
    add basic entity for Engine

---
+++ src/main/java/com/datasphere/core/common/BaseEntity.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2019, Huahuidata, Inc.
+ * DataSphere is licensed under the Mulan PSL v1.
+ * You can use this software according to the terms and conditions of the Mulan PSL v1.
+ * You may obtain a copy of Mulan PSL v1 at:
+ * http://license.coscl.org.cn/MulanPSL
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR
+ * PURPOSE.
+ * See the Mulan PSL v1 for more details.
+ */
+
+package com.datasphere.core.common;
+
+import java.util.Date;
+
+import com.datasphere.resource.manager.common.mybatis.plugin.Pager;
+
+public abstract class BaseEntity {
+	protected String id;
+	protected Date createTime;
+	protected Date lastModified;
+    Pager pager;
+	String sortField;
+	String orderBy;
+	
+	public String getSortField() {
+		return sortField;
+	}
+	public void setSortField(String sortField) {
+		this.sortField = sortField;
+	}
+	public String getOrderBy() {
+		return orderBy;
+	}
+	public void setOrderBy(String orderBy) {
+		this.orderBy = orderBy;
+	}
+	
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public Date getCreateTime() {
+		return createTime;
+	}
+
+	public void setCreateTime(Date createTime) {
+		this.createTime = createTime;
+	}
+
+	public Date getLastModified() {
+		return lastModified;
+	}
+
+	public void setLastModified(Date lastModified) {
+		this.lastModified = lastModified;
+	}
+
+	@SuppressWarnings("rawtypes")
+	public Pager getPager() {
+		return pager;
+	}
+
+	@SuppressWarnings("rawtypes")
+	public void setPager(Pager pager) {
+		this.pager = pager;
+	}
+}
