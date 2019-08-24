d7e1d86e7da1d91d5c510c9e38968b1b7e85fa83
aladin-1001 <aladin1001@126.com>
2019-08-24 11:08:19 +0800

    add runtime exception  Handler
    add Runtime Exception  Handler

---
+++ src/main/java/com/datasphere/engine/common/exception/JRuntimeException.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2019, Huahuidata, Inc.
+ * DataSphere is licensed under the Mulan PSL v1.
+ * You can use this software according to the terms and conditions of the Mulan PSL v1.
+ * You may obtain a copy of Mulan PSL v1 at:
+ * http://license.coscl.org.cn/MulanPSL
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR
+ * PURPOSE.
+ * See the Mulan PSL v1 for more details.
+ */
+
+package com.datasphere.engine.common.exception;
+
+public class JRuntimeException extends RuntimeException
+{
+    private static final long serialVersionUID = 1L;
+    Integer errorCode;
+    
+    public JRuntimeException() {
+        this.errorCode = 1;
+    }
+    
+    public JRuntimeException(final String message) {
+        super(message);
+        this.errorCode = 1;
+    }
+    
+    public JRuntimeException(final Integer errorCode, final String message) {
+        super(message);
+        this.errorCode = errorCode;
+    }
+    
+    public Integer getErrorCode() {
+        return this.errorCode;
+    }
+    
+    public void setErrorCode(final Integer errorCode) {
+        this.errorCode = errorCode;
+    }
+}
