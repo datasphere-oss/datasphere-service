05df0056b1c505f65684a06a699f605a6ac9ed9b
aladin-1001 <aladin1001@126.com>
2019-08-24 19:55:44 +0800

    delete setSession() method
    delete setSession() method

--- src/main/java/com/datasphere/engine/shaker/processor/message/event/service/InstanceEvent.java
+++ src/main/java/com/datasphere/engine/shaker/processor/message/event/service/InstanceEvent.java
@@ -2,9 +2,6 @@ package com.datasphere.engine.shaker.processor.message.event.service;
 
 public interface InstanceEvent {
 	
-
-//	public void setSession(WebSocketSession session) ;
-
 	public void sendToClientMessage() throws Exception ;
 	
 }
