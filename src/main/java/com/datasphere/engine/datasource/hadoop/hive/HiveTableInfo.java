02c1e7c3ea86d988f75fef5781d0856ad276bccc
aladin-1001 <aladin1001@126.com>
2019-08-24 14:38:45 +0800

    add hive table information
    add hive table information

---
+++ src/main/java/com/datasphere/engine/datasource/hadoop/hive/HiveTableInfo.java
@@ -0,0 +1,51 @@
+package com.datasphere.engine.datasource.hadoop.hive;
+
+public class HiveTableInfo {
+
+    private String tableName;
+    private String columns;
+    private String rows;
+    //数据源名称
+    private String name;
+    private String dataDesc;
+
+    public String getTableName() {
+        return tableName;
+    }
+
+    public void setTableName(String tableName) {
+        this.tableName = tableName;
+    }
+
+    public String getColumns() {
+        return columns;
+    }
+
+    public void setColumns(String columns) {
+        this.columns = columns;
+    }
+
+    public String getRows() {
+        return rows;
+    }
+
+    public void setRows(String rows) {
+        this.rows = rows;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDataDesc() {
+        return dataDesc;
+    }
+
+    public void setDataDesc(String dataDesc) {
+        this.dataDesc = dataDesc;
+    }
+}
