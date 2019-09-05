a9cdc2dcf878f6eb535d0b78018ee5813930687f
theseus yang <theseusyang@192.168.1.21>
2019-09-05 14:38:36 +0800

    change copyright

--- src/main/java/com/datasphere/engine/manager/resource/provider/webhdfs/model/WebHDFSDataSourceInfo.java
+++ src/main/java/com/datasphere/engine/manager/resource/provider/webhdfs/model/WebHDFSDataSourceInfo.java
@@ -1,10 +1,20 @@
+/*
+ * Copyright 2019, Huahuidata, Inc.
+ * DataSphere is licensed under the Mulan PSL v1.
+ * You can use this software according to the terms and conditions of the Mulan PSL v1.
+ * You may obtain a copy of Mulan PSL v1 at:
+ * http://license.coscl.org.cn/MulanPSL
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR
+ * PURPOSE.
+ * See the Mulan PSL v1 for more details.
+ */
+
 package com.datasphere.engine.manager.resource.provider.webhdfs.model;
 
 import java.util.List;
 
-/**
- * Created by jxm 2018/8/30
- */
+
 public class WebHDFSDataSourceInfo {
 
     //主键
