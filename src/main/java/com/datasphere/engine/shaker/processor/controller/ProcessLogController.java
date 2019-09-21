ab90b9b09d260e64269d1e9bde96d045e0aba4bc
theseus yang <theseusyang@192.168.0.105>
2019-09-21 17:04:01 +0800

    update package importance

--- src/main/java/com/datasphere/engine/shaker/processor/controller/ProcessLogController.java
+++ src/main/java/com/datasphere/engine/shaker/processor/controller/ProcessLogController.java
@@ -23,6 +23,7 @@ import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Map;
 
