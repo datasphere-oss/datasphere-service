e189850122584357e2bf878c18b40e1bd5cfa57a
theseus yang <theseusyang@192.168.0.105>
2019-09-21 17:06:27 +0800

    update package importance

--- src/main/java/com/datasphere/engine/shaker/processor/instance/controller/ComponentInstanceController.java
+++ src/main/java/com/datasphere/engine/shaker/processor/instance/controller/ComponentInstanceController.java
@@ -18,8 +18,10 @@ import java.util.Map;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.datasphere.common.data.Dataset;
 import com.datasphere.core.common.BaseController;
