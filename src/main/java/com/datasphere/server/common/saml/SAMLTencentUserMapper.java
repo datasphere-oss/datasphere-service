12ef7608d1df4a636af55fc219323427033134fe
theseus yang <theseusyang@192.168.0.105>
2019-09-21 17:44:11 +0800

    saml: add tencent cloud supports

---
+++ src/main/java/com/datasphere/server/common/saml/SAMLTencentUserMapper.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2019, Huahuidata, Inc.
+ * DataSphere is licensed under the Mulan PSL v1.
+ * You can use this software according to the terms and conditions of the Mulan PSL v1.
+ * You may obtain a copy of Mulan PSL v1 at:
+ * http://license.coscl.org.cn/MulanPSL
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR
+ * PURPOSE.
+ * See the Mulan PSL v1 for more details.
+ */
+
+package com.datasphere.server.common.saml;
+
+import org.springframework.security.saml.SAMLCredential;
+
+import com.datasphere.server.domain.user.User;
+
+public class SAMLTencentUserMapper extends SAMLUserMapper{
+
+  @Override
+  public User createUser(SAMLCredential samlCredential) {
+    User tencentUser = new User();
+    if(samlCredential != null){
+    		tencentUser.setTel(getAttributeValue(samlCredential, "手机号"));
+    		tencentUser.setFullName(getAttributeValue(samlCredential, "显示名"));
+    		tencentUser.setEmail(getAttributeValue(samlCredential, "电子邮件"));
+    }
+    return tencentUser;
+  }
+}
