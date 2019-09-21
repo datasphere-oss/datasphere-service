d110f22e784fc1ff677780e8d750b1cb7be0e96e
theseus yang <theseusyang@192.168.0.105>
2019-09-21 17:08:22 +0800

    update package importance

--- src/main/java/com/datasphere/engine/shaker/processor/prep/controller/DataProcessController.java
+++ src/main/java/com/datasphere/engine/shaker/processor/prep/controller/DataProcessController.java
@@ -25,8 +25,10 @@ import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import static com.datasphere.engine.shaker.processor.prep.constant.ReturnConst.DataNotExist;
 import static com.datasphere.engine.shaker.processor.prep.constant.ReturnConst.Failed;
