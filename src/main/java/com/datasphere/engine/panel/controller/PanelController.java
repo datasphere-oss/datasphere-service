a30912b57629ee7286b1c805da068587eefdf14a
theseus yang <theseusyang@192.168.0.105>
2019-09-21 16:58:47 +0800

    update package importance

--- src/main/java/com/datasphere/engine/panel/controller/PanelController.java
+++ src/main/java/com/datasphere/engine/panel/controller/PanelController.java
@@ -28,6 +28,8 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 
