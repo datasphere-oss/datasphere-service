ed59e849546615358b3fb28189c983075337098d
theseus yang <theseusyang@192.168.0.105>
2019-09-22 13:04:08 +0800

    migrate datasource package to datasource project

--- src/main/java/com/datasphere/server/datasource/ingestion/rule/ValidationRule.java
+++
@@ -1,7 +0,0 @@
-package com.datasphere.server.datasource.ingestion.rule;
-
-import com.datasphere.server.spec.druid.ingestion.Validation;
-
-public abstract class ValidationRule {
-  public abstract Validation toValidation(String name);
-}
