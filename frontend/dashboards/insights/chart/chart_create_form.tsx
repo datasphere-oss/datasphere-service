2ead7c1d4ad7ff31f47f4c86499fe577259806a7
theseus yang <theseusyang@192.168.0.106>
2020-01-30 22:39:11 +0800

    add chart-create_form.tsx for insights module
    add chart-create_form.tsx for insights module

---
+++ frontend/dashboards/insights/chart/chart_create_form.tsx
@@ -0,0 +1,26 @@
+import * as React from 'react';
+    + 
+    + export interface TestComponentProps {
+    +     [key: string]: any;
+    + }
+    + 
+    + const TestComponent: React.StatelessComponent<TestComponentProps> = (props) => {
+    +     return (
+    +         <div className=\"control-group\">
+    +             <label className=\"control-label\">源数据集</label>
+    +             <div className=\"controls\">
+    +                 <div
+    +                     object-picker=\"insight.params.datasetSmartName\"
+    +                     type=\"DATASET\"
+    +                     object=\"hook.sourceObject\"
+    +                     error-scope=\"$parent.$parent\"
+    +                 />
+    +                 <input type=\"hidden\" value={insight.params.datasetSmartName} required={true} />{' '}
+    +                 {/* Make the form invalid when no dataset is selected */}
+    +                 <div insight-source-info=\"\" />
+    +             </div>
+    +         </div>
+    +     );
+    + };
+    + 
+    + export default TestComponent;
